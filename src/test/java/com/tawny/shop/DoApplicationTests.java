998bd9551b744b06995c9116f38857b042af36a8
DESKTOP-NJKHAHU\user <mile@naver.com>
2023-08-25 21:03:14 +0900

    (원영도) .최초 파일 생성 및 설정

---
+++ src/test/java/com/tawny/shop/DoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tawny.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
